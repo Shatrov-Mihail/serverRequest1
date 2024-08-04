7aeffa9d35deb5f84182c74a7c81c8b56643c3e2
Shatrov-Mihail <78844887@mail.ru>
2024-08-04 16:08:40 +0500

    Готовое первое задание

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,25 @@
-
+import {useEffect, useState} from 'react'
 import styles from './app.module.css'
 
 export const App = () => {
+	const  [task, setTask] = useState([])
+
+	useEffect(()=> {
+		fetch('http://localhost:3005/task')
+		.then ((loadedData) => loadedData.json())
+		.then((loadedTask)=> {
+			setTask(loadedTask)
+		})
+	}, [])
+
   return (
     <div className={styles.app}>
-Hello
+		<h1 className={styles.header}>Список дел</h1>
+		<ul className={styles.list}>
+		{task.map(({id, name, title})=>(
+		<li key={id} className={styles.listItem}>{name} : {title} </li>
+		))}
+		</ul>
     </div>
   );
 };
